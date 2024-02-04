97adb7493482a5c9fc4ba8fa71f3e705821a9a9a
Franco Calle <francocalle93@gmail.com>
2024-02-03 21:23:44 -0600

    update

--- code/03_run_income_prediction_gradient_boosting.py
+++ code/03_run_income_prediction_gradient_boosting.py
@@ -153,7 +153,7 @@ lasso = Lasso()
 # Define the parameter grid
 # param_grid = {'alpha': [0.0001, 0.0002, 0.0005, 0.001, 0.005, 0.01]}
 # param_grid = {'alpha': [0.00005, 0.0001, 0.001]}
-param_grid = {'alpha': [0.001]}
+param_grid = {'alpha': [0.0001]}
 
 all_params = list(ParameterGrid(param_grid))
 
