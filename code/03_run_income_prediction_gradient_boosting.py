573504e5bc886c2378dec02899d5b93aff4d6a2d
Franco Calle <francocalle93@gmail.com>
2024-02-03 20:08:19 -0600

    update

--- code/03_run_income_prediction_gradient_boosting.py
+++ code/03_run_income_prediction_gradient_boosting.py
@@ -190,6 +190,7 @@ gb_model = GradientBoostingRegressor()
 param_grid = {
     # 'n_estimators': [25,100, 200, 300],
     # 'n_estimators': [500, 600, 700],
+    # 'n_estimators': list(range(100, 1000, 50)),
     'n_estimators': [300],
     # 'learning_rate': [0.01, 0.1]
     # 'n_estimators': [100],
