74c3925193c6c807d7a902a2c0c49167189046a2
Franco Calle <francocalle93@gmail.com>
2024-02-05 05:34:55 -0600

    update

--- code/04_generate_prediction_report_gb.py
+++ code/04_generate_prediction_report_gb.py
@@ -90,10 +90,10 @@ X_standardized_validation['const'] = 1
 # 5. Predict income
 
 ml_dataset_filtered_train['log_income_pc_hat'] = best_model_gb.predict(X_standardized_train)
-ml_dataset_filtered_train['income_pc_hat'] = np.exp(ml_dataset_filtered_train['log_income_pc_hat'] * scaler_Y_train.scale_[0] + scaler_Y_train.mean_[0]) -20
+ml_dataset_filtered_train['income_pc_hat'] = np.exp(ml_dataset_filtered_train['log_income_pc_hat'] * scaler_Y_train.scale_[0] + scaler_Y_train.mean_[0]) 
 
 ml_dataset_filtered_validation['log_income_pc_hat'] = best_model_gb.predict(X_standardized_validation)
-ml_dataset_filtered_validation['income_pc_hat'] = np.exp(ml_dataset_filtered_validation['log_income_pc_hat'] * scaler_Y_train.scale_[0] + scaler_Y_train.mean_[0]) -20
+ml_dataset_filtered_validation['income_pc_hat'] = np.exp(ml_dataset_filtered_validation['log_income_pc_hat'] * scaler_Y_train.scale_[0] + scaler_Y_train.mean_[0]) 
 
 
 # 5. Compiling both datasets and creating some variables:
